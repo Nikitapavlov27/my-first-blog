a8c5796f7f7257a6315fe1aad054b1b5caa4164e
nikitapavlov <nikitapavlov27@mail.ru>
2021-04-04 19:43:56 +0300

    Static trouble 10

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
